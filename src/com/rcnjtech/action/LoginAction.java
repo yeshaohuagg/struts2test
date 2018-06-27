6d2b0222c02ba7becbb9d70480940654227a43f2
yeshaohua <827109043@qq.com>
2018-06-27 18:30:38 +0800

    数据库

--- src/com/rcnjtech/action/LoginAction.java
+++ src/com/rcnjtech/action/LoginAction.java
@@ -2,17 +2,45 @@ package com.rcnjtech.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class LoginAction extends ActionSupport {
     private String username;
     private String password;
 
     @Override
     public String execute() throws Exception {
-        if (username.equals("admin") && password.equals("123")){
-            return SUCCESS;
-        } else {
-            return LOGIN;
+        String ret = ERROR;
+        Connection connection = null;
+        try {
+            String URL = "jdbc:mysql://localhost/struts_tutorial";
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(URL,"root","xufenfen5201314");
+            String sql = "SELECT name FROM login WHERE";
+            sql+=" user = ? AND password = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1,username);
+            ps.setString(2,password);
+            ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()){
+                username = resultSet.getString(1);
+                ret = SUCCESS;
+            }
+        }catch (Exception e){
+            ret = ERROR;
+        } finally {
+            if (connection != null){
+                try {
+                    connection.close();
+                } catch (Exception e){
+
+                }
+            }
         }
+        return ret;
     }
 
     public String getUsername() {
